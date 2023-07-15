7a93235543c4840611f1854b7555c179da16c23f
malvyan24 <alpianmuhamad21@gmail.com>
2023-07-15 19:49:13 +0700

    Project UAS2

--- praktikum10/resources/views/admin/layout/menu.blade.php
+++ praktikum10/resources/views/admin/layout/menu.blade.php
@@ -5,7 +5,7 @@
                     <div class="sb-sidenav-menu">
                         <div class="nav">
                             <div class="sb-sidenav-menu-heading">Core</div>
-                            <a class="nav-link" href="#">
+                            <a class="nav-link" href="{{ url('/dashboard') }}">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Dashboard
                             </a>
@@ -19,13 +19,13 @@
                             <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav">
                                     
-                                    <a class="nav-link" href="#">Kategori Produk</a>
+                                    <a class="nav-link" href="{{ url('/produk') }}">Kategori Produk</a>
                                     <a class="nav-link" href="#">Kumpulan Produk</a>
 
                                 </nav>
                             </div>
                             
-                          
+                        
                     
                 </nav>
             </div>
--- praktikum10/routes/web.php
+++ praktikum10/routes/web.php
@@ -4,6 +4,9 @@
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdukController;
+use App\Http\Controllers\TampilamController;
+use Illuminate\Support\Facades\Auth;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,22 +19,37 @@ use App\Http\Controllers\ProdukController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [TampilamController::class, 'index']);
+
+// bikin routing untuk validasi login user data
+Route::group(['middleware' => ['auth']], function () {
+    Route::prefix('admin')->group(function () {
+
+
+        // routing ke dashboard
+        Route::get('/dashboard', [DashboardController::class, 'index']);
+
+        //routing ke produk
+        Route::get('/produk', [ProdukController::class, 'index']);
+        // routing ke form create
+        Route::get('/produk/create', [ProdukController::class, 'create']);
+        // routing tambah data fungsi store
+        Route::post('/produk/store', [ProdukController::class, 'store']);
+        // routing ke form edit
+        Route::get('/produk/edit/{id}', [ProdukController::class, 'edit']);
+        //routing fungsi update
+        Route::put('/produk/update/{id}', [ProdukController::class, 'update']);
+        // routing fungsi delete data fungsi destroy
+        Route::get('/produk/delete/{id}', [ProdukController::class, 'destroy']);
+    });
 });
 
-// routing ke dashboard
-Route::get('/dashboard', [DashboardController::class, 'index']);
 
-//routing ke produk
-Route::get('/produk', [ProdukController::class, 'index']);
-// routing ke form create
-Route::get('/produk/create', [ProdukController::class, 'create']);
-// routing tambah data fungsi store
-Route::post('/produk/store', [ProdukController::class, 'store']);
-// routing ke form edit
-Route::get('/produk/edit/{id}', [ProdukController::class, 'edit']);
-//routing fungsi update
-Route::put('/produk/update/{id}', [ProdukController::class, 'update']);
-// routing fungsi delete data fungsi destroy
-Route::get('/produk/delete/{id}', [ProdukController::class, 'destroy']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
